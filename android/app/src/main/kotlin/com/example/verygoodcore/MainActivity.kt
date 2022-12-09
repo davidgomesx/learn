431ddade76ba7182dc651a521ac38bb02db04f0c
davidgomesx <david.98g2t@hotmail.com>
2022-12-09 12:43:04 +0000

    First Commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:learn/app/app.dart';
+import 'package:learn/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
